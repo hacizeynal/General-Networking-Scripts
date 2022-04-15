0e41807143dceda197126dadde615428b5bd4df1
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-15 19:29:30 +0200

    v0.1 collect_only_up_interfaces script

--- collect_only_up_interfaces.py
+++ collect_only_up_interfaces.py
@@ -3,6 +3,12 @@ from pathlib import Path
 
 folder_for_device_outputs = Path("device_outputs")
 file_to_open = folder_for_device_outputs / "rn2oumsw001_show_interface_status.txt"
-with file_to_open.open() as f:
-    print(f.read())
+regex_to_catch = ""
 
+
+def collect_only_up_interfaces(device_input, result_output):
+    with open(device_input) as f, open(result_output, "w") as k:
+        return None
+
+
+collect_only_up_interfaces(file_to_open, "final_output.csv")
