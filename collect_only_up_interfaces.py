efd15f58aef7761f845f469491eb45e3e3c45a46
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-16 14:13:01 +0200

    v0.2 collect_only_up_interfaces

--- collect_only_up_interfaces.py
+++ collect_only_up_interfaces.py
@@ -1,6 +1,8 @@
 import re
 import csv
+import os
 
+csv_path = "/Users/zhajili/Desktop/Python/General Networking Scripts/csv_outputs" # path
 regex_for_up_interfaces = r"(?P<hostname>\S+)#show"  # regex to catch all up interfaces and hostname of device
 regex_pattern = re.compile(regex_for_up_interfaces)
 
@@ -9,8 +11,13 @@ def collect_only_up_interfaces(initial_config):
     with open(initial_config) as f:
         match = regex_pattern.search(f.read())
         if match:
-            with open(match.group(1)+"_int_status.csv","w") as k:
-                return None
+            filename = os.path.join(csv_path,match.group(1) + "_int_status.csv") # save filename to directory ,here
+            # hostname is taken via regex pattern and concatenated with string to get final file name ,os.path is
+            # used to get path directory + filename.
+            with open(filename,"w") as k:
+                return "King Julien"
+        else:
+            print("NOTHING MATCHED!")
 
 
 if __name__ == "__main__":
