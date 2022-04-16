cc938847b24418337f4b291d500c885b64afe6be
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-16 22:23:11 +0200

    v1.0 collect only up interfaces

--- collect_only_up_interfaces.py
+++ collect_only_up_interfaces.py
@@ -5,17 +5,24 @@ import os
 csv_path = "/Users/zhajili/Desktop/Python/General Networking Scripts/csv_outputs"  # path
 regex_for_up_interfaces_vlan = r"(?P<interface>\S+) +(?P<description>\S+.\S+.\S+)(?P<status> connected) +(?P<vlan>\d+)"
 regex_for_hostname = r'(?P<hostname>\S+)#'
+resource_list = []
 
 
 def collect_only_up_interfaces(initial_config):
     with open(initial_config) as f:
         for line in f:
             hostname = re.match(regex_for_hostname, line)
-            interface_vlan_status = re.search(regex_for_up_interfaces_vlan, line)
+            interface_vlan_description = re.finditer(regex_for_up_interfaces_vlan, line)
             if hostname:
                 filename = os.path.join(csv_path, hostname.group(1) + "_int_status.csv")  # save filename to directory
-            elif interface_vlan_status:
-                print(interface_vlan_status.group(1),interface_vlan_status.group(2),interface_vlan_status.group(4))
+            elif interface_vlan_description:
+                for i in interface_vlan_description:
+                    resource_list.append(["Interface","Description","Status","VLAN"])
+                    resource_list.append(i.groups())
+                with open(filename, "w") as m:
+                    writer = csv.writer(m)
+                    for row in resource_list:
+                        writer.writerow(row)
 
 
 if __name__ == "__main__":
