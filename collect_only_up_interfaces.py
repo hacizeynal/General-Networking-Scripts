5fb008b05dbd3e2f49d960a2e47891063ea5ff8d
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-18 00:33:40 +0200

    v1.1 fixed regex bug ,fixed headers

--- collect_only_up_interfaces.py
+++ collect_only_up_interfaces.py
@@ -3,9 +3,9 @@ import csv
 import os
 
 csv_path = "/Users/zhajili/Desktop/Python/General Networking Scripts/csv_outputs"  # path
-regex_for_up_interfaces_vlan = r"(?P<interface>\S+) +(?P<description>\S+.\S+.\S+)(?P<status> connected) +(?P<vlan>\d+)"
+regex_for_up_interfaces_vlan = r"(?P<interface>\S+) +(?P<description>\S+.\S+.\S+)(?P<status> +connected) +(?P<vlan>\d+)"
 regex_for_hostname = r'(?P<hostname>\S+)#'
-resource_list = []
+resource_list = [ ]
 
 
 def collect_only_up_interfaces(initial_config):
@@ -17,13 +17,17 @@ def collect_only_up_interfaces(initial_config):
                 filename = os.path.join(csv_path, hostname.group(1) + "_int_status.csv")  # save filename to directory
             elif interface_vlan_description:
                 for i in interface_vlan_description:
-                    resource_list.append(["Interface","Description","Status","VLAN"])
                     resource_list.append(i.groups())
-                with open(filename, "w") as m:
-                    writer = csv.writer(m)
-                    for row in resource_list:
-                        writer.writerow(row)
+        resource_list.insert(0,["Interface", "Description", "Status", "VLAN"])
+        with open(filename, "w") as m:
+            writer = csv.writer(m)
+            for row in resource_list:
+                writer.writerow(row)
+        with open(filename,"r") as k:
+            reader = csv.reader(k)
+            for row in reader:
+                print(row)
 
 
 if __name__ == "__main__":
-    collect_only_up_interfaces("device_outputs/rn2oumsw433.txt")
+    collect_only_up_interfaces("device_outputs/rn2dmzsw459.txt")
